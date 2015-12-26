dae3b44ce16831fc6f0e669290c765729154339b
Robin Wilson <robin@rtwilson.com>
2015-12-26 22:34:29 +0000

    Tidying & PEP8

--- web6s.py
+++ web6s.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, render_template, redirect, url_for, request, session, g
+from flask import Flask, render_template, redirect, url_for, request, session
 from StringIO import StringIO
 import sys
 sys.path.append('/usr/local/lib/python2.6/dist-packages')
@@ -19,75 +19,73 @@ app.secret_key = 'gfen3483krjgnporjregiwnw[pr9jr;ef938r\;\;['
 @app.route('/')
 @app.route('/index')
 def index():
-  if 'id' not in session:
-    import uuid
-    session['id'] = uuid.uuid4()
-  return render_template('index.html')
-#   return repr(sys.prefix)
+    if 'id' not in session:
+        import uuid
+        session['id'] = uuid.uuid4()
+    return render_template('index.html')
+
 
 @app.route('/py6sparams', methods=["POST", "GET"])
 def params():
-  aot = float(request.form['aot'])
-  return run(aot)
+    aot = float(request.form['aot'])
+    return run(aot)
+
 
 @app.route('/py6s/<aot>')
 def run(aot):
-  s = SixS()
-  s.altitudes.set_target_sea_level()
-  s.altitudes.set_sensor_satellite_level()
-  s.ground_reflectance = GroundReflectance.HomogeneousLambertian(GroundReflectance.GreenVegetation)
-  s.aot550 = float(aot)
-  results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.1, output_name="apparent_radiance")
+    s = SixS()
+    s.altitudes.set_target_sea_level()
+    s.altitudes.set_sensor_satellite_level()
+    s.ground_reflectance = GroundReflectance.HomogeneousLambertian(GroundReflectance.GreenVegetation)
+    s.aot550 = float(aot)
+    results = SixSHelpers.Wavelengths.run_vnir(s, spacing=0.1, output_name="apparent_radiance")
 
-  xs = ",".join(map(str, list(results[0])))
-  ys = ",".join(map(str, list(results[1])))
+    xs = ",".join(map(str, list(results[0])))
+    ys = ",".join(map(str, list(results[1])))
 
-  return redirect(url_for('show', x=xs, y=ys, params="none", lab="AOT = %.2f" % s.aot550))
+    return redirect(url_for('show', x=xs, y=ys, params="none", lab="AOT = %.2f" % s.aot550))
+
 
 @app.route('/show/<x>/<y>')
 @app.route('/show/<x>/<y>/<params>')
 @app.route('/show/<x>/<y>/<params>/<lab>')
-def show(x, y, params = "", lab=""):
-  if params == "clear":
-    clf()
-  xvals = map(float, x.split(','))
-  yvals = map(float, y.split(','))
+def show(x, y, params="", lab=""):
+    if params == "clear":
+        clf()
+    xvals = map(float, x.split(','))
+    yvals = map(float, y.split(','))
 
-  print "Label is:"
-  print lab
+    path = '/tmp/%s.pickle' % session['id']
+    if os.path.exists(path):
+        fig, axes = pickle.load(file(path))
+    else:
+        fig, axes = subplots()
 
+    colors = ['blue', 'red', 'green', 'orange', 'black']
+    n_lines = len(axes.lines)
 
-  path = '/tmp/%s.pickle' % session['id']
-  if os.path.exists(path):
-    fig, axes = pickle.load(file(path))
-  else:
-    fig, axes = subplots()
+    axes.plot(xvals, yvals, color=colors[n_lines], label=lab)
+    axes.legend()
 
+    axes.set_xlabel("Wavelength ($\mu m$)")
+    axes.set_ylabel("Radiance ($W/m^2/sr$)")
 
-  colors = ['blue', 'red', 'green', 'orange', 'black']
-  n_lines = len(axes.lines)
+    pickle.dump((fig, axes), file(path, 'w'))
 
-  axes.plot(xvals, yvals, color=colors[n_lines], label=lab)
-  axes.legend()
+    img = StringIO()
+    fig.savefig(img)
+    img.seek(0)
 
-  axes.set_xlabel("Wavelength ($\mu m$)")
-  axes.set_ylabel("Radiance ($W/m^2/sr$)")
+    return img.getvalue().encode("base64").strip()
 
-  pickle.dump((fig, axes), file(path, 'w'))
-
-  img = StringIO()
-  fig.savefig(img)
-  img.seek(0)
-  #return send_file(img, mimetype='image/png')
-  return img.getvalue().encode("base64").strip()
 
 @app.route('/clear')
 def clear():
-  path = '/tmp/%s.pickle' % session['id']
+    path = '/tmp/%s.pickle' % session['id']
 
-  os.remove(path)
-  del session['id']
-  redirect('index')
+    os.remove(path)
+    del session['id']
+    redirect('index')
 
 if __name__ == '__main__':
-  app.run(debug = True)
+    app.run(debug=True)
